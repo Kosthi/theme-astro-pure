3e3dfc3295e263260ba74e65c5152a9aabd83384
Koschei <nitianzero@gmail.com>
2025-01-09 00:02:19 +0800

    feat: 更新个人基本信息

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const theme: ThemeUserConfig = {
   /** Title for your website. Will be used in metadata and as browser tab title. */
   title: 'Astro Theme Pure',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'Koschei',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: '人的梦想是不会结束的',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
@@ -59,8 +59,8 @@ export const theme: ThemeUserConfig = {
   footer: {
     // Registration information for ICP (optional)
     registration: {
-      url: 'https://icp.gov.moe/?keyword=APTX4869',
-      text: 'Moe ICP APTX4869'
+      url: 'https://beian.miit.gov.cn/',
+      text: '浙ICP备2024075396号-2'
     },
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: true,
